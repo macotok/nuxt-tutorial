382f0e6a1fcb37462f619eca6c5be075e3fc7ff7
kikuchi-makoto <kikuchi-makoto5@aeonpeople.biz>
2018-12-10 16:19:01 +0900

    use asyncData github

--- pages/_user.vue
+++ pages/_user.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <div>
-      {{$route.params.user}}
+      <img :src="avatar_url" width="100">
+      <span>
+        {{name}}
+      </span>
     </div>
+    <a :href="html_url">
+      {{html_url}}
+    </a>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'user',
+    async asyncData({ app, params }) {
+      if (process.server) {
+        console.log('server')
+      }
+      if (process.client) {
+        console.log('client')
+      }
+      const data = await app.$axios.$get(`https://api.github.com/users/${params.user}`)
+      return data;
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,9 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        sample-app
-      </h1>
-      <h2 class="subtitle">
-        Hello! Nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <ul>
+      <li><a href="/macotok">ダイレクトリンク</a></li>
+      <li><nuxt-link to="macotok">ルータリンク</nuxt-link></li>
+    </ul>
   </section>
 </template>
 
